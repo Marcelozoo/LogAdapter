78e535e1fe426b135dbe7167645957e9de72ea42
Marcelozoo <marcelo.pinheiro01@hotmail.com>
2024-03-30 19:32:01 -0300

    first commit

---
+++ src/main/java/com/ufes/logadapter/IArquivoLog.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ufes.logadapter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+public interface IArquivoLog {
+    public void salvarLog(String msg, String operacao, String nome, LocalDate data, LocalTime hora,boolean usuarioAutenticado);
+    public boolean verificaTipoArquivo(String tipoArquivo);
+}
---
+++ src/main/java/com/ufes/logadapter/LogAdapter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ufes.logadapter;
+
+/**
+ *
+ * @author ascherit
+ */
+public class LogAdapter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
